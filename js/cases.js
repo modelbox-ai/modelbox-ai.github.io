4aa78a4e117839b8f32f3867efb7986cf556d472
Rachel329 <wuwanqi1@huawei.com>
2021-10-30 11:33:33 +0800

    req:主页2.0版本

---
+++ js/cases.js
@@ -0,0 +1,31 @@
+cases = JSON.parse(`[
+
+]`);
+
+
+/*
+	{
+		"title": "智慧城市",
+		"content": "阿大达娃大碗大碗大碗的法务无法让莪方式而发给大哥v啊是大二上发的如果爱的色放如果啊多少人发给啊但仍然嘎儿风格啊嘎儿啊二人噶额的风格",
+		"icon": "./img/case-icon1.png",
+		"backgroudImage": "./img/case1.png",
+		"label": {
+			"title": "智慧城市",
+			"items": ["行人车辆检测", "垃圾检测"]
+		},
+		"buttons": [
+			{
+				"text": "重要按钮",
+				"clazz": "banner-button-use",
+				"id": "aiCityMain",
+				"show": false
+			},
+			{
+				"text": "次要按钮",
+				"type": "normal",
+				"clazz": "banner-button-details",
+				"show": false
+			}
+		]
+	}
+*/
---
+++ js/course.js
@@ -0,0 +1,57 @@
+course = JSON.parse(`[
+		{
+			"title": {
+				"label": "阶段一：ModelBox入门课程",
+				"content": "介绍如何快速上手，使用AI应用开发与运行框架ModelBox来开发AI应用。",
+				"poster": "./img/course1.png",
+				"footer": {
+					"left": "",
+					"right": {
+						"text": "",
+						"url": ""
+					}
+				}
+			},
+			"content": [
+			]
+		}
+]`);
+
+
+/*
+		{
+			"title": {
+				"label": "阶段一：ModelBox入门课程",
+				"content": "介绍如何快速上手，使用AI应用开发与运行框架ModelBox来开发AI应用。",
+				"poster": "./img/course1.png",
+				"footer": {
+					"left": "",
+					"right": {
+						"text": "",
+						"url": ""
+					}
+				}
+			},
+			"content": [
+				{
+					"icon": "./img/course1.svg",
+					"title": "course1",
+					"labels": [{
+						"text": "label1",
+						"color": "#666"
+					}, {
+						"text": "label2",
+						"color": "red"
+					}],
+					"date": "2021-09-29",
+					"address": "daDAWD",
+					"content": "",
+					"btn": {
+						"text": "立即购买",
+						"show": false,
+						"css": {}
+					}
+				}
+			]
+		}
+*/
